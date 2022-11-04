9ce4f49eb5d31b6443b3b81f0c2324e2925204fd
Remi Denise <remi.denise@gmail.com>
2022-11-04 09:18:25 +0000

    :tada: First commit

--- workflow/scripts/merge_tab.py
+++ workflow/scripts/merge_tab.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 
-for file_tab in input.all_out :
+for file_tab in snakemake.input.all_out :
     if os.stat(file_tab).st_size != 0 :
         with open(file_tab, "rt") as r_file :
             header = r_file.readline().rstrip().replace("#", "")
             break
 
-with open(str(output), "wt") as w_file :
+with open(snakemake.output.out, "wt") as w_file :
     w_file.write(header+"\n")
 
     for file_out in snakemake.input.all_out : 
